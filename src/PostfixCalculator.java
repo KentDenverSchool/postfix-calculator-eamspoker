4d2944bb2c31480308eed26ce9085a9d9774548f
Emily Amspoker <eamspoker21@kentdenver.org>
2019-10-21 13:14:59 -0600

    added instance data, began calculate

--- src/PostfixCalculator.java
+++ src/PostfixCalculator.java
@@ -1,2 +1,15 @@
 public class PostfixCalculator {
+    private static Stack mainStack;
+
+    public static int calculate(String equation){
+        mainStack = new Stack();
+        String[] instructions = equation.split(" ");
+
+        System.out.println(instructions[2]);
+        return 0;
+
+    }
+    
+
+
 }
